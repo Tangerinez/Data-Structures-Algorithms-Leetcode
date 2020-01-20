6efda7fa2aeaf9231bb0f5cc566223c40fea8cbf
Gregory Tang <gtang159@gmail.com>
2020-01-19 17:05:16 -0800

    Updated quicksort algorithm

--- Concepts/Algorithms/Sorting/Quick Sort/index.js
+++ Concepts/Algorithms/Sorting/Quick Sort/index.js
@@ -1,3 +1,18 @@
+/*
+Divide and Conquer
+// Pick a pivot, and have all numbers less than that pivot go to the left, and all numbers greater than that pivot go to the right. Repeat on each divided list until list is sorted
+
+// Useful for sorting large data sets
+// Usually the fastest sorting method (along with merge sort) on average, but the worst case runtime is slow!
+
+Runtime Complexity: 
+  - Best Case: O(nlogn)
+  - Worst Case: O(n^2) => if the pivot is the largest item in the list
+Space Complexity: O(logn)
+*/
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
 // Helper function to swap array items
 const swap = (array, leftIndex, rightIndex) => {
   let temp = array[leftIndex];
@@ -7,7 +22,7 @@ const swap = (array, leftIndex, rightIndex) => {
 
 // Helper function to partition array elements
 const partition = (array, leftIndex, rightIndex) => {
-  let pivot = array[Math.floor((rightIndex + leftIndex) / 2)], // middle element
+  let pivot = array[Math.floor((rightIndex + leftIndex) / 2)], // we're using a middle pivot here
     i = leftIndex, //left pointer
     j = rightIndex; //right pointer
   while (i <= j) {
@@ -34,7 +49,7 @@ const partition = (array, leftIndex, rightIndex) => {
 const quickSort = (array, leftIndex, rightIndex) => {
   let currentIndex;
   if (array.length > 1) {
-    currentIndex = partition(array, leftIndex, rightIndex); //index returned from partition
+    currentIndex = partition(array, leftIndex, rightIndex); // index returned from partition
     if (leftIndex < currentIndex - 1) {
       // more elements on the left side of the pivot
       quickSort(array, leftIndex, currentIndex - 1);
@@ -46,3 +61,6 @@ const quickSort = (array, leftIndex, rightIndex) => {
   }
   return array;
 };
+
+quickSort(numbers, 0, numbers.length - 1);
+console.log(numbers);
