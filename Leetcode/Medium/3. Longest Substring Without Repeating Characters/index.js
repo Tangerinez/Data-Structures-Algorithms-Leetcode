7c67a77bf9fc99dfccd4a3e895c55a514b41983f
Gregory Tang <gtang159@gmail.com>
2020-01-27 21:33:33 -0800

    Longest substring with no repeating char answer

--- Leetcode/Medium/3. Longest Substring Without Repeating Characters/index.js
+++ Leetcode/Medium/3. Longest Substring Without Repeating Characters/index.js
@@ -20,3 +20,20 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3. 
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+
+var lengthOfLongestSubstringHashMap = function(s) {
+  let hashMap = {}, // hash that keeps track of non-repeated char and their positions
+    start = 0, // pointer that tracks beginning index of non-repeated char substring
+    max = 0; // stores max length of non-repeating substring
+  for (let i = 0; i < s.length; i++) {
+    if (hashMap[s[i]] !== null) {
+      if (hashMap[s[i]] + 1 > start) {
+        // if the character is after start
+        start = hashMap[s[i]] + 1; // set new start pointer to current index
+      }
+    }
+    hashMap[s[i]] = i; // update char position
+    max = i - start + 1 > max ? i - start + 1 : max; // update max if neccessary
+  }
+  return max;
+};
